f71c7e57a4b2cc73ed5d65988390e391e9b0b50d
Thomas J. Leeper <thosjleeper@gmail.com>
2016-07-20 18:28:05 +0200

    new func: consume_msg() (closes #5)

--- R/messages.r
+++ R/messages.r
@@ -1,10 +1,13 @@
-#' @title Receive message
-#' @description Receive one or more messages from an SQS queue
+#' @rdname receive_msg
+#' @title Receive messages
+#' @description Receive one or more messages from an SQS queue.
+#' @details \code{receive_msg} simply receives message(s). \code{consume_msg} does the same and then deletes the message(s) from the queue.
 #' @param queue A character string containing a queue URL, or the name of the queue.
 #' @param attributes Currently ignored.
 #' @param n The number of messages to retrieve (maximum 10).
 #' @param timeout A number of seconds to make the message invisible to subsequent \code{receive_msg} requests. This modifies the queue's default visibility timeout. See \code{\link{visibility}} to modify this value after receiving a message.
 #' @param wait A number of seconds to wait for messages before responding to the request.
+#' @param query A list specifying additional query arguments to be passed to the \code{query} argument of \code{\link{sqsHTTP}}.
 #' @param ... Additional arguments passed to \code{\link{sqsHTTP}}.
 #' @return A data.frame of messages.
 #' @author Thomas J. Leeper
@@ -12,9 +15,9 @@
 #' @references
 #' \href{http://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_ReceiveMessage.html}{ReceiveMessage}
 #' @export
-receive_msg <- function(queue, attributes = NULL, n = 1, timeout = NULL, wait = NULL, ...) {
+receive_msg <- function(queue, attributes = NULL, n = 1, timeout = NULL, wait = NULL, query = NULL, ...) {
     queue <- .urlFromName(queue)
-    query_args <- list(Action = "ReceiveMessage")
+    query_args <- c(query, list(Action = "ReceiveMessage"))
     if (n > 10) {
         query_args$MaxNumberOfMessages <- 10L
         warning("Maxmimum 'n' exceeded so 10 used by default")
@@ -51,6 +54,15 @@ receive_msg <- function(queue, attributes = NULL, n = 1, timeout = NULL, wait = 
               RequestId = out$ReceiveMessageResponse$ResponseMetadata$RequestId)
 }
 
+#' @rdname receive_msg
+#' @param receive_args A named list of arguments, other than \code{queue}, to be passed to \code{receive_msg}.
+#' @param delete_args A named list of arguments, other than \code{queue} and \code{handle}, to be passed to \code{\link{delete_msg}}.
+#' @export
+consume_msg <- function(queue, receive_args = list(), delete_args = list()) {
+    m <- do.call("receive_msg", c(receive_args, list(queue = queue)))
+    d <- do.call("delete_msg", c(delete_args, list(queue = queue, handle = m$ReceiptHandle)))
+    m
+}
 
 
 #' @title delete_msg
@@ -58,6 +70,7 @@ receive_msg <- function(queue, attributes = NULL, n = 1, timeout = NULL, wait = 
 #' @details Delete one or more messages from an SQS queue. If a message is not deleted, it remains visible in the queue and will be returned by subsequent calls to \code{\link{receive_msg}}.
 #' @param queue A character string containing a queue URL, or the name of the queue.
 #' @param handle A message handle, as returned by \code{\link{receive_msg}}.
+#' @param query A list specifying additional query arguments to be passed to the \code{query} argument of \code{\link{sqsHTTP}}.
 #' @param ... Additional arguments passed to \code{\link{sqsHTTP}}.
 #' @return If operation succeeds, a logical \code{TRUE}.
 #' @author Thomas J. Leeper
@@ -67,11 +80,11 @@ receive_msg <- function(queue, attributes = NULL, n = 1, timeout = NULL, wait = 
 #' 
 #' \href{http://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_DeleteMessageBatch.html}{DeleteMessageBatch}
 #' @export
-delete_msg <- function(queue, handle, ...) {
+delete_msg <- function(queue, handle, query = NULL, ...) {
     queue <- .urlFromName(queue)
     if (length(handle) > 1) {
         # batch mode
-        query_args <- list(action = "DeleteMessageBatch")
+        query_args <- c(query, list(action = "DeleteMessageBatch"))
         n <- 1:length(handle)
         id <- paste0("msg", n)
         a <- as.list(c(id, handle))
@@ -80,7 +93,7 @@ delete_msg <- function(queue, handle, ...) {
         query_args <- c(query_args, a)
     } else {
         # single mode
-        query_args <- list(ReceiptHandle = handle, Action = "DeleteMessage")
+        query_args <- c(query, list(ReceiptHandle = handle, Action = "DeleteMessage"))
     }
     out <- sqsHTTP(url = queue, query = query_args, ...)
     if (inherits(out, "aws-error") || inherits(out, "unknown")) {
@@ -97,6 +110,7 @@ delete_msg <- function(queue, handle, ...) {
 #' @param msg A character vector containing one or more message bodies.
 #' @param attributes Currently ignored. (If \code{msg} is of length one, a specification of message attributes. Ignored otherwise.)
 #' @param delay A numeric value indicating the number of seconds between 0 and 900 to delay a specific message. If \code{NULL}, the default value for the queue applies.
+#' @param query A list specifying additional query arguments to be passed to the \code{query} argument of \code{\link{sqsHTTP}}.
 #' @param ... Additional arguments passed to \code{\link{sqsHTTP}}.
 #' @return A list of message information, including the MessageId and an MD5 checksum of the message body.
 #' @author Thomas J. Leeper
@@ -105,7 +119,7 @@ delete_msg <- function(queue, handle, ...) {
 #' \href{http://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_SendMessage.html}{SendMessage}
 #' \href{http://docs.aws.amazon.com/AWSSimpleQueueService/latest/APIReference/API_SendMessageBatch.html}{SendMessageBatch}
 #' @export
-send_msg <- function(queue, msg, attributes = NULL, delay = NULL, ...) {
+send_msg <- function(queue, msg, attributes = NULL, delay = NULL, query = NULL, ...) {
     queue <- .urlFromName(queue)
     if(length(msg) > 1) {
         # batch mode
@@ -126,7 +140,7 @@ send_msg <- function(queue, msg, attributes = NULL, delay = NULL, ...) {
         # single mode
         query_args <- list(Action = "SendMessage")
         query_args$MessageBody = msg
-        out <- sqsHTTP(url = queue, query = query_args, ...)
+        out <- sqsHTTP(url = queue, query = c(query, query_args), ...)
         if (inherits(out, "aws-error") || inherits(out, "unknown")) {
             return(out)
         }
